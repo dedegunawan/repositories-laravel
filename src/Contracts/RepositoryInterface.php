45dcd66c43637d3eaf4e2e02df03445c18c967c5
Dede Gunawan <dede@unsil.ac.id>
2017-07-01 20:23:29 +0700

    Add ; on RepositoryInterface

--- src/Contracts/RepositoryInterface.php
+++ src/Contracts/RepositoryInterface.php
@@ -2,19 +2,19 @@
 namespace Dede\Repositories\Contracts;
 
 interface RepositoryInterface {
-    public function all($columns = array('*'))
+    public function all($columns = array('*'));
 
-    public function paginate($perPage = 15, $columns = array('*'))
+    public function paginate($perPage = 15, $columns = array('*'));
 
-    public function create(array $data)
+    public function create(array $data);
 
-    public function update(array $data, $id)
+    public function update(array $data, $id);
 
-    public function delete($id)
-    
-    public function find($id, $columns = array('*'))
+    public function delete($id);
 
-    public function findBy($field, $value, $columns = array('*'))
+    public function find($id, $columns = array('*'));
+
+    public function findBy($field, $value, $columns = array('*'));
 
 
 }
